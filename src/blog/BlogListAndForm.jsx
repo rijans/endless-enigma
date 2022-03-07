b52f14e71b52a56b14165c7d47c62742fe8cba4e
Jaber Al Nahian <rijans.x@gmail.com>
2022-03-08 01:14:58 +0600

    Fixes

--- src/blog/BlogListAndForm.jsx
+++ src/blog/BlogListAndForm.jsx
@@ -100,7 +100,7 @@ function BlogListAndForm(props) {
                 <div className="mr-5">
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Blog
                         Title</label>
-                    <input type="text" id="title"
+                    <input type="text" id="title" name={'title'}
                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                            placeholder="Title.." required value={blogPageState.input_title}
                            onChange={handleFormChange}/>
@@ -108,9 +108,9 @@ function BlogListAndForm(props) {
                 <div className="mr-5">
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Blog
                         Content</label>
-                    <input type="text" id="content"
+                    <input type="text" id="content" name={'content'}
                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                           required value={blogPageState.input_content} onChange={handleFormChange}/>
+                           required value={blogPageState.input_content} onChange={handleFormChange} placeholder={'Content..'}/>
                 </div>
                 <button type="submit"
                         className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{blogPageState.formActionLabel}
